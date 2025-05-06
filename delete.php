1ef73754a991ee44d6531f641ce28a23e2c8ed83
My_precious_Vea <keithyvheaiv@gmail.com>
2025-05-06 18:12:38 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -40,6 +40,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete</title>
+    <link rel="icon" type="image/png" href="newLogo.png">
 </head>
 <body>
     <form action="delete.php" method="post" class="notify">
