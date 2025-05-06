909c2ce46ef2293a76e3ec7e3db2fba13db62013
My_precious_Vea <keithyvheaiv@gmail.com>
2025-05-06 18:07:53 +0800

    Update main.php

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Main Interface</title>
-    <link rel="icon" type="image/jpg" href="OIP.jpg">
+    <link rel="icon" type="image/png" href="newLogo.png">
     <link rel="stylesheet" href="style.css">
 </head>
 <body class="main">
