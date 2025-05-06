b38327dc003ef3f05d3022650e33462209532fbd
My_precious_Vea <keithyvheaiv@gmail.com>
2025-05-06 18:11:44 +0800

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dash Board</title>
+    <link rel="icon" type="image/png" href="newLogo.png">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
 </head>
 <body>
